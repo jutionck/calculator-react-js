410b2cdc330e7ccb1d5bb10be63df89855280c2a
jutionck <jutionck@gmail.com>
2020-07-16 15:27:39 +0700

    Simple calculator with css flex

---
+++ src/appCal/appBackup.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import '../App.css';
+import ResultComponent from "./component/ResultComponent";
+import KeyPadComponent from "./component/KeyPadComponent";
+
+class App extends Component {
+
+    state = {
+        result: ""
+    }
+
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        let checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Simple Calculator</h1>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/component/KeyPadComponent.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import '../assets/css/sytyle.css';
+
+class KeyPadComponent extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="flex-container">
+                    <button style={{width:180, backgroundColor:"green"}} name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                    <button style={{width:"180px", backgroundColor:"#FF8C00"}} name="C" onClick={e => this.props.onClick(e.target.name)}>C</button>
+                </div>
+                <div className="flex-container">
+                    <button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                    <button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                    <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                    <button name="+" onClick={e => this.props.onClick(e.target.name)}>+</button>
+                </div>
+                <div className="flex-container">
+                    <button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+                    <button name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+                    <button name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+                    <button name="-" onClick={e => this.props.onClick(e.target.name)}>-</button>
+                </div>
+                <div className="flex-container">
+                    <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+                    <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+                    <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+                    <button name="*" onClick={e => this.props.onClick(e.target.name)}>*</button>
+                </div>
+
+
+                <div className="flex-container">
+                    <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                    <button name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
+                    <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+                    <button name="/" onClick={e => this.props.onClick(e.target.name)}>/</button>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+
+export default KeyPadComponent;
